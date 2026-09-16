318844d3d54b51b94c9bc29cbdf95fd862331f94
rhonni666-debug <rhonni666@gmail.com>
2026-09-15 21:39:58 -0300

    android: configurar Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rr7.bet',
+  appName: 'RR7.BET Demo',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
