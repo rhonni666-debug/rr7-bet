fe30bdaff6bf94effca1b5b4ddd6e5681748d104
rhonni666-debug <rhonni666@gmail.com>
2026-09-16 10:33:24 -0300

    feat: route RIPCOM B2B admin panel

--- src/router.tsx
+++ src/router.tsx
@@ -15,6 +15,7 @@ import { AdminOperationsPage } from './pages/AdminOperations';
 import { AdminHub88PocPage } from './pages/AdminHub88Poc';
 import { AdminPocMetricsPage } from './pages/AdminPocMetrics';
 import { AdminB2BSandboxPage } from './pages/AdminB2BSandbox';
+import { AdminRipcomPage } from './pages/AdminRipcom';
 import { RipcomPlayerPage } from './pages/RipcomPlayer';
 
 const rootRoute = createRootRoute({ component: () => <AppShell><Outlet /></AppShell> });
@@ -39,6 +40,7 @@ const adminCreditsRoute = createRoute({ getParentRoute: () => rootRoute, path: '
 const adminSessionsRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/sessoes', component: () => <AdminOperationsPage section="sessions" /> });
 const adminTransactionsRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/transacoes', component: () => <AdminOperationsPage section="transactions" /> });
 const adminAuditRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/auditoria', component: () => <AdminPage section="audit" /> });
+const adminRipcomRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/ripcom', component: AdminRipcomPage });
 const adminB2BSandboxRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/b2b-sandbox', component: AdminB2BSandboxPage });
 const adminHub88PocRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/hub88-poc', component: AdminHub88PocPage });
 const adminPocMetricsRoute = createRoute({ getParentRoute: () => rootRoute, path: '/admin/poc-metrics', component: AdminPocMetricsPage });
@@ -65,6 +67,7 @@ const routeTree = rootRoute.addChildren([
   adminSessionsRoute,
   adminTransactionsRoute,
   adminAuditRoute,
+  adminRipcomRoute,
   adminB2BSandboxRoute,
   adminHub88PocRoute,
   adminPocMetricsRoute,
