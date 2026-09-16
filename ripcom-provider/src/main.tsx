2f28d94b6ed5f436f2c731c47eb450e4b6db67c0
rhonni666-debug <rhonni666@gmail.com>
2026-09-16 12:10:08 -0300

    feat: mount enhanced Eclipse Serpent player for game sessions

--- ripcom-provider/src/main.tsx
+++ ripcom-provider/src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { EclipsePlayer } from './game/EclipsePlayer';
 import { GameVfxMount } from './game/GameVfxMount';
 import './styles.css';
 import './game-vfx.css';
 import './cinematic-vfx.css';
+import './bonus-mode.css';
+
+const params = new URLSearchParams(window.location.search);
+const playerToken = params.get('play');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <GameVfxMount />
+    {playerToken ? <EclipsePlayer token={playerToken} /> : <><App /><GameVfxMount /></>}
   </React.StrictMode>,
 );
