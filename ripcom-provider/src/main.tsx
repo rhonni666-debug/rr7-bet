e7154539d77a93dd282d7323390048a2bbbce7f9
rhonni666-debug <rhonni666@gmail.com>
2026-09-16 11:58:18 -0300

    feat: mount cinematic Eclipse Serpent visual components

--- ripcom-provider/src/main.tsx
+++ ripcom-provider/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { GameVfxMount } from './game/GameVfxMount';
 import './styles.css';
 import './game-vfx.css';
+import './cinematic-vfx.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <GameVfxMount />
   </React.StrictMode>,
 );
